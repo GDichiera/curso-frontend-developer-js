f46dd0e701cd2431b94e7b0570a871f2265dd369
GDichiera <gisela12650@gmail.com>
2023-11-15 20:52:23 -0300

    La pari pero fusione la clase 6

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 menuHamIcon.addEventListener("click", toggleMobileMenu)
@@ -32,3 +33,58 @@ function toggleProductDetail(){
     } 
     aside.classList.toggle("inactive")
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://battle.com.ar/battle/img/prod/mtb/241M/01.webp",
+})
+productList.push({
+    name: 'Pantalla',
+    price: 500,
+    image: "https://img.freepik.com/vector-premium/pantalla-monitor-computadora_1297-434.jpg",
+})
+productList.push({
+    name: 'Computadora',
+    price: 1000,
+    image: "https://pronotebooks.com.ar/wp-content/uploads/2023/09/lenovo-3i-1-600x600.webp",
+})
+
+function recorrerArrayDePruductos(arr){
+    for (product of arr) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card")
+        const productImg = document.createElement("img")
+        //con .setAtributte le vamos a asignar valor al atributo src, y le vamos a decir que ese valor va a ser la direccion que este en la propiedad imagen de cada iteracion del array
+        productImg.setAttribute('src', product.image) //se pone ("atributo", valor)
+      
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info")
+    
+        const productInfoDiv = document.createElement("div")
+        
+        const productPrice = document.createElement("p")
+        productPrice.innerText = `$${product.price}`
+        const productName = document.createElement("p")
+        productName.innerText = product.name
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+        const productInfoFigure = document.createElement("figure")   
+        const productImgCart = document.createElement("img")
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
+      
+        productInfoFigure.appendChild(productImgCart)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+      
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+recorrerArrayDePruductos(productList)
