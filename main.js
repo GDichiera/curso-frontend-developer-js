326f4476ab914b43cb2d22f6c2925bfa64e754ed
GDichiera <gisela12650@gmail.com>
2023-11-15 18:03:09 -0300

    fusione clase 8 al index

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menuHamIcon.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu(){
 //     esta funcion busca la clase inactative en destop-menu y si la tiene se la quita y si no la tiene se la agrega (eso lo hace classList.toggle)
     desktopMenu.classList.toggle("inactive")
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive")
 }
